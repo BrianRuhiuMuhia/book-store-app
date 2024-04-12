098610d3cb7a05e38c2a94d21b6e62c6a5859cf3
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2024-04-12 17:55:49 +0300

    frontend tailwind

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express=require("express")
 const app=express()
 const dotenv=require("dotenv")
+const cors=require("cors")
 const port=process.env.PORT || 5000
-const insert=require("./insertData.js")
 const router=require("./router/router.js")
 app.use(express.json())
+app.use(cors());
+app.disable("x-powered-by"); //Reduce fingerprinting
 app.use(router)
 app.listen(port,()=>{
     console.log(`server running on port ${port}`)
